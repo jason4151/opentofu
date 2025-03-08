359bf45429c12c60c9d7bf48476c00357e0587bf
Jason Moskowitz <jason4151@users.noreply.github.com>
2025-03-08 17:00:20 -0600

    Create outputs.tf

---
+++ vpc/outputs.tf
@@ -0,0 +1,85 @@
+# vpc/outputs.tf
+
+# Output the VPC ID for use in other configurations
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+# Output the public subnet IDs as a list
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+# Output the private subnet IDs as a list
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+# Output the public route table ID
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+# Output the private route table IDs as a list
+output "private_route_table_ids" {
+  description = "List of private route table IDs"
+  value       = aws_route_table.private[*].id
+}
+
+# Output the Internet Gateway ID
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+# Output the NAT Gateway IDs as a list
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = aws_nat_gateway.nat[*].id
+}
+
+# Output the S3 VPC Endpoint ID
+output "s3_vpc_endpoint_id" {
+  description = "ID of the S3 VPC Endpoint"
+  value       = aws_vpc_endpoint.s3.id
+}
+
+# Output the DynamoDB VPC Endpoint ID
+output "dynamodb_vpc_endpoint_id" {
+  description = "ID of the DynamoDB VPC Endpoint"
+  value       = aws_vpc_endpoint.dynamodb.id
+}
+
+# Output the Network ACL ID for downstream rule modifications
+output "nacl_id" {
+  description = "ID of the Network ACL"
+  value       = aws_network_acl.main.id
+}
+
+# Output the VPC CIDR Block
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+# Output the AWS Account ID
+output "account_id" {
+  description = "AWS Account ID"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+# Output the ID of the ECR API VPC Endpoint
+output "ecr_api_endpoint_id" {
+  description = "ID of the ECR API VPC Endpoint"
+  value       = aws_vpc_endpoint.ecr_api.id
+}
+
+# Output the ID of the ECR DKR VPC Endpoint
+output "ecr_dkr_endpoint_id" {
+  description = "ID of the ECR DKR VPC Endpoint"
+  value       = aws_vpc_endpoint.ecr_dkr.id
+}
