1b706b1024c864ca081bf38ba99da6139700fee7
Jason Moskowitz <jason4151@users.noreply.github.com>
2025-03-08 13:57:06 -0600

    analytics.yml workflow

--- analytics/provider.tf
+++ analytics/provider.tf
@@ -1,13 +1,11 @@
 # analytics/provider.tf
+
 provider "aws" {
   region = "us-east-2"
 }
 
 terraform {
   backend "s3" {
-    bucket         = "opentofu-state-bucket-jason4151"
-    key            = "analytics/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "opentofu-state-lock-jason4151"
+    # Bucket, key, region, and dynamodb_table provided by workflow secrets
   }
 }
