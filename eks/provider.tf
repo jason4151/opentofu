f05ca290bca1aa545decd66fda513c0d66b7cec0
Jason Moskowitz <jason4151@users.noreply.github.com>
2025-03-08 18:25:40 -0600

    Update workflows (#45)
    * s3_state_bucket updates
    
    * Rename analytics
    
    * Update iam-deploy.yml
    
    * Update iam-deploy.yml
    
    * Update iam-deploy.yml
    
    * Update iam-deploy.yml
    
    * Update iam-deploy.yml
    
    Additional annotations.
    
    * Update vpc-deploy.yml
    
    Added additional features to vpc-deploy.yml.
    
    * Update ec2-jump-box-deploy.yml
    
    * Update ec2-jump-box-deploy.yml
    
    * Delete outputs.tf
    
    * Update iam-deploy.yml
    
    * Delete outputs.tf
    
    * Update vpc-deploy.yml
    
    * Update eks-deploy.yml
    
    * Create outputs.tf
    
    * Update analytics-deploy.yml
    
    * Delete outputs.tf
    
    * Added create-all and destroy-all workdflows
    
    * Added worfklow_call ability
    
    * Update analytics-deploy.yml
    
    * Update analytics-deploy.yml
    
    * Update destroy-all.yml
    
    * Create outputs.tf
    
    * Update destroy-all.yml
    
    * Update analytics-deploy.yml
    
    * Remove concurrency from workflows
    
    * Update analytics-deploy.yml
    
    * Updates

---
+++ eks/provider.tf
@@ -0,0 +1,11 @@
+# eks/provider.tf
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    # Bucket, key, region, and dynamodb_table provided by workflow secrets
+  }
+}
